ed99654290cb2155834b77952d512bf1a7b1b45f
Carlos Moya <cmoya212@gmail.com>
2019-07-24 16:53:00 -0400

    Update nugetpack.

--- nugetpack.bat
+++ nugetpack.bat
@@ -7,36 +7,6 @@ cd ..
 cd Cflashsoft.Framework.AppServices
 nuget pack -prop Configuration=Release
 
-rem cd ..
-
-rem cd Cflashsoft.Framework.Optimization
-rem nuget pack -prop Configuration=Release
-
-cd ..
-
-cd Cflashsoft.Framework.Security
-nuget pack -prop Configuration=Release
-
-cd ..
-
-cd Cflashsoft.Framework.SecurityCore
-nuget pack -prop Configuration=Release
-
-cd ..
-
-cd Cflashsoft.Framework.SqlUtility
-nuget pack -prop Configuration=Release
-
-cd ..
-
-cd Cflashsoft.Framework.Http
-nuget pack -prop Configuration=Release
-
-cd ..
-
-cd Cflashsoft.Framework.Web.WebUtility
-nuget pack -prop Configuration=Release
-
 cd ..
 
 cd Cflashsoft.Framework.EntityAppServices
@@ -50,11 +20,6 @@ nuget pack -prop Configuration=Release
 cd ..
 
 cd Cflashsoft.Framework.WcfAppServices
-nuget pack -prop Configuration=Release
-
-cd ..
-
-cd Cflashsoft.Framework.AspNetCore.Identity
 nuget pack -prop Configuration=Release
 
 cd ..
